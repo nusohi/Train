486afb56c3b0d87779f3117504a2cc9ce6b720a9
yyspyysp <45526192+yyspyysp@users.noreply.github.com>
2018-12-03 14:48:06 +0800

    完善劫匪寻路 孩子哭 及转头

--- Assets/Scripts/Children.cs
+++ Assets/Scripts/Children.cs
@@ -5,18 +5,36 @@ using UnityEngine;
 public class Children : MonoBehaviour
 {
     public bool isCrying = false;
-    public float CryTimer = 0;
+    public float CryTimer = 0;            //哭的冷却时间
     public float CryTime = 10f;
+
+    public float StayTimer = 0;            //劫匪停留时间
+    public float StayTime = 5f;
 
     void Update()
     {
+        if (KidnapperMove._intance.navAgent.remainingDistance < 0.5f)
+        {
+               StayTimer += Time.deltaTime;
+        }
         if (isCrying == true)
+        {
             CryTimer += Time.deltaTime;
+            
+        }
+
         if (CryTimer >= CryTime)
         {
             CryTimer = 0;
             isCrying = false;
+           
+        }
+
+        if (StayTimer >= StayTime)
+        {
+           
             KidnapperMove._intance.GameState = KidnapperMove.Patroling;
+            StayTimer = 0;
         }
     }
 
