02c6a91647dca67cff359ca6538ff97bb75a512d
wangtianci <377090139@qq.com>
2018-12-03 17:47:38 +0800

    过渡动画 结局切换 场景改变

---
+++ Assets/SceneFadeInOut.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneFadeInOut : MonoBehaviour {
+    public static SceneFadeInOut Instance;
+    public float fadeSpeed = 1.5f;
+    public bool sceneStarting = true;
+    private RawImage rawImage;
+	// Use this for initialization
+	void Awake () {
+        rawImage = GetComponent<RawImage>();
+        Instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (sceneStarting)
+            StartScene();
+	}
+    private void FadeToClear()
+    {
+        rawImage.color = Color.Lerp(rawImage.color, Color.clear, fadeSpeed * Time.deltaTime);
+    }
+    private void FadeToBlack()
+    {
+        rawImage.color = Color.Lerp(rawImage.color, Color.black, fadeSpeed * Time.deltaTime);
+    }
+    void StartScene()
+    {
+        FadeToClear();
+        if(rawImage.color.a<0.05f)  //当透明度足够则场景开始
+        {
+            rawImage.color = Color.clear;
+            rawImage.enabled = false;
+            sceneStarting = false;
+        }
+    }
+    public void EndScene()
+    {
+        rawImage.enabled = true;
+        FadeToBlack();
+        //if(rawImage.color.a>0.95f)
+        //{
+        //    SceneManager.LoadScene("Introduction");
+        //}
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,20 +4,22 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour {
+    
     //实例
     public static GameManager Instance;
     //车厢长度
     public static float CarriageLength;
-
+    //UI
     public float Countdown = 60.0f;//倒计时
     public Text CountDownText;
+
 
     //大人
     private int AdultNumber = 7;
     private string CurrentCharacter;
     public int CurrentID;
     public Adult[] Adults;
-    public bool[] AdultDie;
+    public bool AdultDie=false;
     //劫匪A
     public Kidnapper Kidnappers;
     public bool KidnapperDie;
@@ -27,8 +29,6 @@ public class GameManager : MonoBehaviour {
     //狗
     public Dog dog;
     public bool DogDie=false;
-    //小孩
-    // public Children[] children;
 
 
 
@@ -46,40 +46,48 @@ public class GameManager : MonoBehaviour {
 
 
 	// Use this for initialization
-	void Awake () {
+	void Start () {
         Instance = this;
         for(int i=0;i<Adults.Length;i++)
         {
             Adults[i].ID = i;
         }
-	}
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
         //判断结局
-        if(Countdown>0)
+        if(KidnapperDie&&bossdie)//劫匪全死
         {
-            //倒计时相关
-            Countdown -= Time.deltaTime;//更新倒计时
-            CountDownText.text = "Time  :  " + (int)Countdown;
-        }
-        else
-        {
-            if(KidnapperDie==false&&bossdie==false)//劫匪都没死
+            if(AdultDie||DogDie)//至少有一人或狗死亡
             {
-                NormalEnd = true;   //NormalEnd场景会跳转到badend
+                NormalEnd = true;
             }
-            else if(KidnapperDie==true&&bossdie==true)  //劫匪全死
+            else//没人死亡
             {
                 TrueEnd = true;
             }
+        }
+        else
+        {
+            if(Countdown>0)
+            {
+                //倒计时相关
+                Countdown -= Time.deltaTime;//更新倒计时
+                CountDownText.text = "Time  :  " + (int)Countdown;
+            }
             else
             {
-                BadEnd = true;
+                if(KidnapperDie==false||bossdie==false)//劫匪没全死
+                {
+                    BadEnd = true;   //NormalEnd场景会跳转到badend
+                }
             }
+
         }
-        
+        GameOver();
         
         //鼠标点击
         MousePosition = Input.mousePosition;
@@ -125,13 +133,10 @@ public class GameManager : MonoBehaviour {
         }
         
 
-        //结局相关
-
-
 	}
    
 
-    //判断游戏结局
+    //游戏结局
     public void GameOver()
     {
 
@@ -162,7 +167,7 @@ public class GameManager : MonoBehaviour {
     {
         if(tag=="Adult")    //大人死
         {
-            AdultDie[ID] = true;
+            AdultDie = true;
             Adults[ID].GetComponent <Adult> ().enabled = false;
             return;
         }
